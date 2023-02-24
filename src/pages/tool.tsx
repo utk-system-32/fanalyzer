f22e317053660d2234945a16c968edc70b1ce9a6
Jake Woodard <jakewoodard@Jakes-MacBook-Air-5.local>
2023-02-24 12:51:00 -0500

    tool page created

---
+++ src/pages/tool.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Layout from "../components/Layout";
+const Tool: NextPage = () => {
+  return (
+    <Layout
+      pageTitle="Graphing Tool"
+      metaDescription="Out-graph the competition."
+    >
+      Test
+    </Layout>
+  );
+};
+
+export default Tool;
