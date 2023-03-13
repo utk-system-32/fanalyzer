a5710f901e0bf5014904c55a0f11fa264d5fccd3
Jake Woodard <jakewoodard@Jakes-MacBook-Air-6.local>
2023-03-13 14:09:06 -0500

    small dash header changes

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,1 +1,15 @@
+import { type NextPage } from "next";
+import Layout_Dashboard from "../components/Layout_Dashboard";
 
+const Feed: NextPage = () => {
+  return (
+    <Layout_Dashboard
+      pageTitle="Fanalyzer | Feed"
+      metaDescription="Find your competitive edge."
+    >
+
+    </Layout_Dashboard>
+  );
+};
+  
+export default Feed;
