f40e3ba4ce9c977ea59789a2febd35e4fbd34cd7
Nolan Askew <nolanaskew@gmail.com>
2023-03-14 16:10:14 -0400

    added create post functionality to save created posts to database

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,6 +2,19 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
+
+  createPost: publicProcedure
+   .input(z.object({ title: z.string(), content: z.string() }))
+   .mutation(({ ctx, input }) => {
+    return ctx.prisma.post.create({
+      data: {
+        authorId: ctx.session?.user?.id,
+        title: input.title,
+        content: input.content,
+      }
+    })
+  }),
+
   getByUser: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.post.findMany({
       where: {
