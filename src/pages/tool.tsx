e062ad41cffa97e59d91eeeb7aa77206400839b1
Trevor Mangrum <25257772+trevormangrum@users.noreply.github.com>
2023-03-14 16:47:36 -0400

    Adds file upload functionality

--- src/pages/tool.tsx
+++ src/pages/tool.tsx
@@ -1,30 +1,38 @@
 import { type NextPage } from "next";
-import Layout_Dashboard from "../components/Layout_Dashboard";
-import Head from "next/head";
-import { title } from "process";
-import Image from "next/image";
 import Dropdown from "src/components/Dropdown";
 import Link from "next/link";
+import { type SyntheticEvent, useRef } from "react";
 
 const Tool: NextPage = () => {
+  const inputFile = useRef<HTMLInputElement | null>(null);
+  const openDataset = (e: SyntheticEvent) => {
+    e.preventDefault()
+    if(inputFile.current) {
+      inputFile.current.click();
+    }
+
+  };
   return (
-    <main className="flex w-full flex-col">
-      <div className="flex w-full flex-col border-b">
-        <nav className="flex w-full max-w-[1280px] flex-row self-center  [&>div]:mx-3">
-          <Dropdown dropdownButtonText="File">
-            <button>New Visualization</button>
-            <button>Open Dataset</button>
-          </Dropdown>
-          <Dropdown dropdownButtonText="Edit">
-            <button>Undo</button>
-            <button>Redo</button>
-          </Dropdown>
-          <Link href="/dashboard" className="ml-auto">
-            Return to Dashboard
-          </Link>
-        </nav>
-      </div>
-    </main>
+    <>
+      <main className="flex w-full flex-col">
+        <div className="flex w-full flex-col border-b">
+          <nav className="flex w-full max-w-[1280px] flex-row self-center  [&>div]:mx-3">
+            <Dropdown dropdownButtonText="File">
+              <button>New Visualization</button>
+              <button onClick={openDataset}>Open Dataset</button>
+            </Dropdown>
+            <Dropdown dropdownButtonText="Edit">
+              <button>Undo</button>
+              <button>Redo</button>
+            </Dropdown>
+            <Link href="/dashboard" className="ml-auto">
+              Return to Dashboard
+            </Link>
+          </nav>
+        </div>
+        <input type='file' id='file' ref={inputFile} style={{ display: 'none' }} />
+      </main>
+    </>
   );
 };
 
