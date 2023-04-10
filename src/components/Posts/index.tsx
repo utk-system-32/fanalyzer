08b97351be740119a8b91c2981457ac01a3f4205
Jake Woodard <jakewoodard@Jakes-MacBook-Air-8.local>
2023-04-10 02:03:49 -0400

    progress on feed output layout

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,5 +1,6 @@
 import { api } from '../../utils/api'
 
+
 function Posts(mode) {
     // get all/following/my posts depending on mode
     const date: Date = new Date();
@@ -18,18 +19,42 @@ function Posts(mode) {
     <div>
       {postQuery.data?.map((post) => {
         const date = post.createdAt
+
         dateString = date.toString();
         //const nameString = api.user.getUserByID.useQuery(post.authorId)
         const nameString = "John Doe"
-        const emptyString = " "
+        const numLikes = 5
+
+        const tempString = numLikes.toString()
+        const likeString = tempString + " likes"
+
+        // store todays datetime to compare
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // check for timezone of post, remove it 
+        // and adjust based on timezone the user is in currently
+        const dateWithoutTimezone = new Date(date.getTime() - (date.getTimezoneOffset() * 60 * 1000));
+        dateString = dateWithoutTimezone.toISOString().replace(/\.\d{3}Z$/, "");
+
+        // if the date of post is not today
+        if (date < today) {
+          // Date is not today, so remove the time so it only prints the date
+          const dateWithoutTime = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+          dateString = dateWithoutTime.toDateString();
+        }
         
+        // visualization should go in between post title and content
+        // profile pic at the top left of the username
         return (
           <article key={post.id}>
-            <p>{nameString}</p>
-            <p className="font-bold">{post.title}</p>
-            <p>{post.content}</p>
-            <p>{dateString}</p>
-            <p>{emptyString}</p>
+            <div className="h-full w-80 overflow-y-auto border-2 border-[#ff8200] bg-[#ededed] px-4 py-1">
+              <p className="font-semibold">{nameString}</p>
+              <p className="text-3xl">{post.title}</p>
+              <p>{post.content}</p>
+              <p className="text-right text-xs">{dateString}</p>
+              <p className= "text-right text-xs text-[#ff8200] font-bold">{likeString}</p>
+            </div>
           </article>
         )
       })}
