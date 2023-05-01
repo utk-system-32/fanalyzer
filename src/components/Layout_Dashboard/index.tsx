f323b6bb2e2b765af637c8346fcd0728b00f872e
Nolan Askew <71944967+nolanaskew@users.noreply.github.com>
2023-05-01 16:20:12 -0400

    adds delete post and unlike functionality (#36)
    Co-authored-by: Nolan Askew <nolanaskew@gmail.com>

--- src/components/Layout_Dashboard/index.tsx
+++ src/components/Layout_Dashboard/index.tsx
@@ -25,7 +25,7 @@ const Layout_Dashboard: FunctionComponent<Props> = ({
     <>
       <Head>
         <title>{pageTitle}</title>
-        <meta name="description" content={metaDescription} />
+        <meta name="viewport" content="width=1024" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="relative flex min-h-screen w-full  flex-col items-center">
